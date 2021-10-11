35ee3e1be0073f520f04ae33ec33c6f5c499e87f
Vasily Makarov <vasily.makarov@ultratendency.com>
2021-10-11 20:00:31 +0300

    Working with values

--- DataBaseService.py
+++ DataBaseService.py
@@ -21,7 +21,7 @@ def read_dataframe(path):
 
     result = pd.DataFrame.from_records([s.to_dict() for s in result_lst])
     result = result.sort_values('ts')
-    print(result)
+    #print(result)
     return result
 
 
@@ -32,7 +32,7 @@ def compute_dfs(df, entity_class):
         for index, row in group.iterrows():
             if row['op'] == 'c':
                 current_entity = entity_class(row['data'], row['ts'])
-                print(f"Created current object: {current_entity.to_dict()}")
+                #print(f"Created current object: {current_entity.to_dict()}")
 
                 result_rows.append(current_entity.to_dict())
             else:
@@ -42,7 +42,7 @@ def compute_dfs(df, entity_class):
                 for member in members:
                     if getattr(temp_entity, member) is not None:
                         setattr(current_entity, member, getattr(temp_entity, member))
-                        print(f"Updated current member {member}: {current_entity.to_dict()}")
+                        #print(f"Updated current member {member}: {current_entity.to_dict()}")
 
                 result_rows.append(current_entity.to_dict())
 
@@ -60,18 +60,42 @@ cards_history_df = compute_dfs(cards_logs_df, Card)
 savings_accounts_logs_df = read_dataframe("data/savings_accounts")
 savings_accounts_history_df = compute_dfs(savings_accounts_logs_df, SavingAccount)
 
+# а что если смерджить все три в одну, и выделять группы по
+# можно добавить тогда сразу один ивент туда в кардс для проверки
+
+#res = accounts_history_df.merge(cards_history_df, on=['card_id'], how='outer', suffixes=('', '_card'))
+res = savings_accounts_history_df.merge(accounts_history_df, on=['savings_account_id'], how='left', suffixes=('', '_savings_account'))
+#res = res.merge(cards_history_df, on=['card_id'], how='outer', suffixes=('', '_card_id'))
+res = res.sort_values('ts_savings_account')
+# далее группируем по [account_id, savings_account_id, ts_acc] и находим максимальный ts в группе но который меньше чем
+# (если такой записи нет, то по идее вставляет NAN, но в этом плане пофиг поскольку если есть ссылка то должна быть эта
+print('======')
+print(res)
+
+grouped = res.groupby(['account_id', 'savings_account_id', 'ts_savings_account'], dropna=False)
+lst=[]
+for cols, group in grouped:
+    if len(group) == 1:
+        lst.append(group.iloc[0])
+        continue
+
+    closest_row = group[group['ts_savings_account'] >= group[f'ts_savings_account']].sort_values(f'ts_savings_account').iloc[0]
+    lst.append(closest_row)
+
+print(pandas.DataFrame.from_records(lst))
 
 def join_hist_table(accounts_df, join_df, join_id, suffix):
     res = accounts_df.merge(join_df, on=[join_id], how='left', suffixes=('', suffix))
+    #print(res)
     cols = accounts_df.columns.tolist()
     res = res.groupby(cols, dropna=False)
     resul = []
     for name, group in res:
         not_n = group[group['ts'] >= group[f'ts{suffix}']].sort_values(f'ts{suffix}')
-        print("----" + f"sufix {suffix}")
-        print(cols)
-        print(name)
-        print(not_n)
+        #print("----" + f"sufix {suffix}")
+        #print(cols)
+        #print(name)
+        #print(not_n)
 
         if len(not_n) > 0:
             r = group.loc[not_n[f'ts{suffix}'].idxmax()]
@@ -83,9 +107,22 @@ def join_hist_table(accounts_df, join_df, join_id, suffix):
             #print(group.iloc[0])
             resul.append(group.iloc[0])
     joined_df = pd.DataFrame.from_records(resul).sort_values('ts')
-    print(joined_df)
+    #print(joined_df)
     return joined_df
 
 
-joined = join_hist_table(accounts_history_df, cards_history_df, 'card_id', '_card')
-result_join = join_hist_table(joined, savings_accounts_history_df, 'savings_account_id', '_acc')
+joined = join_hist_table(cards_history_df, accounts_history_df, 'card_id', '_card')
+result_join = join_hist_table(savings_accounts_history_df, accounts_history_df, 'savings_account_id', '_acc')
+
+
+# а что если идти row by row и просто находить каждый раз нужный row для мерджа
+# но тут сложности с тем, что три разные таблицы и типа у них разные истори
+
+
+# так а может заджойнить к cardTable аккаунты, и к savingAccount table аккаунты.
+# а может забить на те, где есть na и заджойнить только это. А na потом union сделать
+# я бы сказал еще что уникальный id - это [account_id, ts], [card_id, ts_card], [account_saving_id, ts_account]
+# если мы джойним к cardTable -> accountTable
+# card_id ts_card account_id ts_account
+# далее мы джойним к account_savingTable -> accountTable
+# account_saving_id ts_account_sabing account_id ts_account
